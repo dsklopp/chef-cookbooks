1e19be038dab53df60ddedd80a1784abee66233c
git <dsklopp@gmail.com>
2014-08-31 15:57:59 -0700

    Initial creation of repository

---
+++ cookbooks/xen/metadata.rb
@@ -0,0 +1,7 @@
+name             'xen'
+maintainer       'Daniel Klopp'
+maintainer_email 'dsk_l_o_p_p gmail.com omit_the_underscores'
+license          'MIT'
+description      'Installs/Configures xen'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
